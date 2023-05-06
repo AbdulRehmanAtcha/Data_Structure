a4ac08f198ee25db3751b13d0431ef4777d3f53f
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2023-05-06 13:13:47 +0500

    Search in Linked List Done

--- Linked-List.c
+++ Linked-List.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 struct Node
 {
     int data;
@@ -45,18 +46,36 @@ void traverse(struct Node *head)
     printf("\n");
     printf("\n");
 }
+struct Node* search(struct Node *head, int data)
+{
+    struct Node* temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == data)
+        {
+            return temp;
+            /* code */
+        }
+        temp = temp->next;
+    
+    }
+    
+    
+}
 
 int main()
 {
-    int choice, data;
+    int choice, data, toSearch;
     struct Node *head = NULL;
+    struct Node* searchResult;
 
     while (1)
     {
         printf("\nLinked List Operations:\n");
         printf("1. Insert\n");
         printf("2. Traverse\n");
-        printf("3. Exit\n");
+        printf("3. Search\n");
+        printf("0. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -71,13 +90,28 @@ int main()
             traverse(head);
             break;
         case 3:
+            printf("Enter the number you wants to search: ");
+            scanf("%d", &toSearch);
+            searchResult = search(head, toSearch);
+            if (searchResult != NULL)
+            {
+                /* code */
+                printf("Found");
+                printf("\n");
+            }
+            else{
+                printf("Not Found");
+                printf("\n");
+
+            }
+            
+            break;
+        case 0:
+            printf("Thankss");
             exit(0);
         default:
             printf("Invalid choice. Please enter a valid option.\n");
         }
     }
-
-
-
     return 0;
 }
